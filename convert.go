34abff710bada2983fa83f31d4756937a5a9c365
lumos <a1439458305@163.com>
2019-04-29 19:07:01 +0800

    11

--- convert.go
+++ convert.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/binary"
+	"goc/toolcom/cfgtool"
 	"goc/toolcom/errtool"
 	"image"
 	"image/color"
@@ -9,49 +11,64 @@ import (
 	"os"
 )
 
-func convert2bin(path string) {
+func convert2bin(path string) ([]byte, int, int) {
+
+	nullColor, err := cfgtool.New("conf.json").TakeInt("NullColor")
+	errtool.Errpanic(err)
+
 	file, err := os.Open(path)
 	errtool.Errpanic(err)
 	defer file.Close()
 	img, _, err := image.Decode(file)
 	errtool.Errpanic(err)
 	bound := img.Bounds()
-	raw := make([]color.Color, 0)
+	raw := make([]color.NRGBA, 0)
 
-	log.Debug("x[%d] y[%d]", bound.Max.X, bound.Max.Y)
 	for y := 0; y < bound.Max.Y; y++ {
 		for x := 0; x < bound.Max.X; x++ {
-			raw = append(raw, img.At(x, y))
+			c := color.NRGBAModel.Convert(img.At(x, y))
+			nrgb := c.(color.NRGBA)
+			raw = append(raw, nrgb)
 		}
 	}
 
-	// rgb16 := make([]uint16, 0)
+	rgb16 := make([]uint16, 0)
 
-	// for _, val := range raw {
-	// 	_r, _g, _b, _a := val.RGBA()
-	// 	var r, g, b, a uint8
-	// 	r = uint8(_r / 255)
-	// 	g = uint8(_g / 255)
-	// 	b = uint8(_b / 255)
-	// 	a = uint8(_a)
-	// 	log.Debug("[%d] [%d] [%d] [%d]", r, g, b, a)
-	// 	if a != 255 {
-	// 		rgb16 = append(rgb16, 0)
-	// 		continue
-	// 	}
-	// 	r >>= 3
-	// 	g >>= 2
-	// 	b >>= 3
-	// 	log.Debug("[%d] [%d] [%d]", r, g, b)
-	// 	var rgb uint16
-	// 	rgb = uint16(r)
-	// 	rgb <<= 6
-	// 	rgb |= uint16(g)
-	// 	rgb <<= 5
-	// 	rgb |= uint16(b)
+	for _, val := range raw {
+		r := val.R >> 3
+		g := val.G >> 2
+		b := val.B >> 3
 
-	// 	rgb16 = append(rgb16, rgb)
-	// }
-	log.Debug("raw[%+v]", raw)
-	// log.Debug("rgb16[%+v]", rgb16)
+		if val.A != 255 {
+			rgb16 = append(rgb16, uint16(nullColor))
+			continue
+		}
+
+		var rgb uint16
+		rgb = uint16(r)
+		rgb <<= 6
+		rgb |= uint16(g)
+		rgb <<= 5
+		rgb |= uint16(b)
+		rgb16 = append(rgb16, rgb)
+
+	}
+
+	endian, err := cfgtool.New("conf.json").TakeString("Endian")
+	errtool.Errpanic(err)
+
+	rgb8 := make([]byte, 0)
+	for _, val := range rgb16 {
+		b8 := make([]byte, 2)
+		switch endian {
+		case "BigEndian":
+			binary.BigEndian.PutUint16(b8, val)
+		case "LittleEndian":
+			binary.LittleEndian.PutUint16(b8, val)
+		default:
+			log.Panic("unknown endian[%s]", endian)
+		}
+		rgb8 = append(rgb8, b8...)
+	}
+	return rgb8, bound.Max.X, bound.Max.Y
 }
--- main.go
+++ main.go
@@ -1,10 +1,151 @@
 package main
 
-import "goc/logface"
+import (
+	"fmt"
+	"goc/logface"
+	"goc/toolcom/errtool"
+	"io/ioutil"
+	"os"
+	"strings"
+)
 
-var log = logface.New(logface.TraceLevel)
+var log = logface.New(logface.DebugLevel)
+
+var fileC *os.File
+var fileH *os.File
+var fileB *os.File
+
+func codeStart() {
+	errtool.Ignore(os.Remove("lt768ui_resources.c"))
+	errtool.Ignore(os.Remove("lt768ui_resources.h"))
+	errtool.Ignore(os.Remove("lt768ui.bin"))
+
+	var err error
+	fileC, err = os.Create("lt768ui_resources.c")
+	errtool.Errpanic(err)
+	fileC.WriteString("#include \"lt768ui_resources.h\"\r\n")
+
+	fileH, err = os.Create("lt768ui_resources.h")
+	errtool.Errpanic(err)
+	fileH.WriteString("#ifndef LT768UI_RESOURCES_H\r\n")
+	fileH.WriteString("#define LT768UI_RESOURCES_H\r\n")
+	fileH.WriteString("#include \"lt768ui_interface.h\"\r\n")
+
+	fileB, err = os.Create("lt768ui.bin")
+	errtool.Errpanic(err)
+}
+
+func codeStop() {
+	fileH.WriteString("#endif")
+
+	fileC.Close()
+	fileH.Close()
+	fileB.Close()
+
+	info, err := os.Stat("lt768ui.bin")
+	errtool.Errpanic(err)
+	log.Info("bin size[%v]", info.Size)
+}
+
+func codeCformat(name string, offset int, w int, h int) string {
+	str := ""
+	str += fmt.Sprintf("dma_info_t %s = {\r\n", name)
+	str += fmt.Sprintf("\t.addr = %d,\r\n", offset)
+	str += fmt.Sprintf("\t.src_w = %d,\r\n", w)
+	str += fmt.Sprintf("\t.x = %d,\r\n", 0)
+	str += fmt.Sprintf("\t.y = %d,\r\n", 0)
+	str += fmt.Sprintf("\t.w = %d,\r\n", w)
+	str += fmt.Sprintf("\t.h = %d,\r\n", h)
+	str += fmt.Sprintf("};\r\n")
+	return str
+}
+
+func codeHformat(name string) string {
+	str := ""
+	str += fmt.Sprintf("extern dma_info_t %s;\r\n", name)
+	return str
+}
+
+var offset = 0
+
+func codeAdd(path string) {
+	log.Debug("handle file[%s]", path)
+	bin, w, h := convert2bin(path)
+	name := strings.Replace(path, "/", "_", -1)
+	name = strings.TrimSuffix(name, ".jpg")
+	name = strings.TrimSuffix(name, ".png")
+	name = strings.TrimSuffix(name, ".jpeg")
+
+	log.Debug("generate name[%s]", name)
+
+	fileB.Write(bin)
+	fileC.WriteString(codeCformat(name, offset, w, h))
+	fileH.WriteString(codeHformat(name))
+
+	offset += len(bin)
+	log.Debug("code add resources[%s]", name)
+}
+
+func judgeBg(dir string) string {
+	bgName := ""
+	files, err := ioutil.ReadDir(dir)
+	errtool.Errpanic(err)
+	for _, file := range files {
+		if !file.IsDir() {
+			nameList := strings.Split(file.Name(), "/")
+			switch nameList[len(nameList)-1] {
+			case "bg.jpg":
+				bgName = dir + "/" + file.Name()
+
+			case "bg.png":
+				bgName = dir + "/" + file.Name()
+
+			case "bg.jpeg":
+				bgName = dir + "/" + file.Name()
+			}
+		}
+	}
+
+	return bgName
+}
+
+func traversalWidget(dir string) {
+	log.Debug("traversalWidget dir[%s]", dir)
+
+	files, err := ioutil.ReadDir(dir)
+	errtool.Errpanic(err)
+	for _, file := range files {
+		if !file.IsDir() {
+			codeAdd(dir + "/" + file.Name())
+		}
+	}
+}
+
+func traversalInterface(dir string) {
+	log.Debug("traversalInterface dir[%s]", dir)
+	bgName := judgeBg(dir)
+	if bgName == "" {
+		log.Panic("find no bg, dir[%s]", dir)
+	}
+	files, err := ioutil.ReadDir(dir)
+	errtool.Errpanic(err)
+	codeAdd(bgName)
+	for _, file := range files {
+		if file.IsDir() {
+			traversalWidget(dir + "/" + file.Name())
+		}
+	}
+}
 
 func main() {
-	convert2bin("close.png")
-	select {}
+	codeStart()
+	files, err := ioutil.ReadDir("./")
+	errtool.Errpanic(err)
+
+	for _, file := range files {
+		if file.IsDir() && !strings.Contains(file.Name(), ".") {
+			traversalInterface(file.Name())
+		}
+	}
+	codeStop()
 }
